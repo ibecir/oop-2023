b2e296dac7ad0df00710ec22fec0c17174d70664
ibecir <becir@bioptimizers.com>
2023-10-21 09:10:34 +0200

    Lectures example for the encapsulation

---
+++ src/main/java/week4/lectures/encapsulation/AccountBalance.java
@@ -0,0 +1,55 @@
+package week4.lectures.encapsulation;
+
+import java.security.InvalidParameterException;
+import java.util.Date;
+
+public class AccountBalance {
+    private int customerId;
+    private int balance;
+    private Date date;
+    private boolean isActive;
+    private CardType cardType;
+
+    public int getCustomerId() {
+        return customerId;
+    }
+
+    public void setCustomerId(int customerId) {
+        this.customerId = customerId;
+    }
+
+    public int getBalance() {
+        return balance;
+    }
+
+    public void setBalance(int balance) {
+        if(balance <= 0)
+            throw new InvalidParameterException("Balance has to be greater than 0!");
+
+        this.balance = balance;
+    }
+
+    public Date getDate() {
+        return date;
+    }
+
+    public void setDate(Date date) {
+        this.date = date;
+    }
+
+    public boolean isActive() {
+        return isActive;
+    }
+
+    public void setActive(boolean active) {
+        isActive = active;
+    }
+
+    public CardType getCardType() {
+        return cardType;
+    }
+
+    public void setCardType(CardType cardType) {
+        this.cardType = cardType;
+    }
+}
---
+++ src/main/java/week4/lectures/encapsulation/CardType.java
@@ -0,0 +1,5 @@
+package week4.lectures.encapsulation;
+
+public enum CardType {
+    CREDIT, DEBIT
+}
