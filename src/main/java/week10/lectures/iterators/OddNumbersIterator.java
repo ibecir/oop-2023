5809460cc9c6953b686b39cbd0caa3e3ec5abdb5
ibecir <becir@bioptimizers.com>
2023-12-02 09:33:45 +0100

    Week 10 materials

---
+++ src/main/java/week10/lectures/iterators/OddNumbersIterator.java
@@ -0,0 +1,39 @@
+package week10.lectures.iterators;
+
+import java.util.ArrayList;
+import java.util.Iterator;
+import java.util.NoSuchElementException;
+
+class OddNumbersIterator implements Iterator<Integer> {
+    private int[] elements;
+    private int currentIndex = 0;
+
+    public OddNumbersIterator(int[] elements) {
+        this.elements = elements;
+    }
+
+    @Override
+    public boolean hasNext() {
+        while (currentIndex < elements.length && elements[currentIndex] % 2 == 0) {
+            currentIndex++;  // Skip even numbers
+        }
+        return currentIndex < elements.length;
+    }
+
+    @Override
+    public Integer next() {
+        if (!hasNext()) {
+            throw new NoSuchElementException();
+        }
+        return elements[currentIndex++];
+    }
+}
+
+class OddNumberIteratorMain{
+    public static void main(String[] args) {
+        int[] numbers = {1,2,3,4,5,6,7,8,9};
+        OddNumbersIterator iterator = new OddNumbersIterator(numbers);
+        while (iterator.hasNext())
+            System.out.println(iterator.next());
+    }
+}
