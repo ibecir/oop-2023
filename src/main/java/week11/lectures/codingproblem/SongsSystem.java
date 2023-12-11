a049b310a6807c0655750b3d58a0372863f6ef46
ibecir <becir@bioptimizers.com>
2023-12-11 07:21:27 +0100

    Week 11 materials

--- src/main/java/week11/lectures/codingproblem/SongsSystem.java
+++ src/main/java/week11/lectures/codingproblem/SongsSystem.java
@@ -5,24 +5,25 @@ import java.io.FileReader;
 import java.io.IOException;
 import java.util.ArrayList;
 import java.util.List;
-import java.util.NoSuchElementException;
 import java.util.Optional;
 import java.util.stream.Collectors;
 
 class SongsSystem {
     List<Song> songs;
-    public SongsSystem(String filename){
+
+    public SongsSystem(String filename) {
         try {
             songs = readSongs(filename);
-        } catch (IOException e){
+        } catch (IOException e) {
             songs = new ArrayList<>();
             System.out.println("Unable to read songs");
         }
     }
+
     public static void main(String[] args) throws IOException {
         SongsSystem system = new SongsSystem("songs.csv");
 
-        for (Song song : system.songs){
+        for (Song song : system.songs) {
             System.out.println(String.valueOf(song.id()) + song.trackName());
         }
 
@@ -37,20 +38,11 @@ class SongsSystem {
 
     public static List<Song> readSongs(String filename) throws IOException {
         List<Song> songs = new ArrayList<>();
-        BufferedReader bufferedReader = new BufferedReader(
-                new FileReader(filename)
-        );
+        BufferedReader bufferedReader = new BufferedReader(new FileReader(filename));
         List<String> lines = bufferedReader.lines().collect(Collectors.toList());
-        for (String line : lines){
+        for (String line : lines) {
             String[] songParts = line.split(",");
-            Song s = new Song(
-                songParts[0] != "" ? Integer.parseInt(songParts[0]) : 000,
-                songParts[1],
-                songParts[2],
-                songParts[3],
-                songParts[4] != "" ? Integer.parseInt(songParts[4]) : 000,
-                songParts[5] != "" ? Integer.parseInt(songParts[5]) : 000
-            );
+            Song s = new Song(songParts[0] != "" ? Integer.parseInt(songParts[0]) : 000, songParts[1], songParts[2], songParts[3], songParts[4] != "" ? Integer.parseInt(songParts[4]) : 000, songParts[5] != "" ? Integer.parseInt(songParts[5]) : 000);
             songs.add(s);
         }
 
@@ -59,43 +51,40 @@ class SongsSystem {
         return songs;
     }
 
-    public Optional<Song> getSongById(int id){
-        for (Song s : songs){
-            if(s.id() == id)
-                return Optional.of(s);
+    public Optional<Song> getSongById(int id) {
+        for (Song s : songs) {
+            if (s.id() == id) return Optional.of(s);
         }
         return Optional.ofNullable(null);
     }
-    public Song getSongByIdNonOptional(int id){
-        for (Song s : songs){
-            if(s.id() == id)
-                return s;
+
+    public Song getSongByIdNonOptional(int id) {
+        for (Song s : songs) {
+            if (s.id() == id) return s;
         }
         throw new EmptySongsListException("Song could not be found");
     }
-    public Song getHighestPopularitySong(){
-        if(songs.size() == 0)
-            throw new EmptySongsListException("List of songs is empty");
+
+    public Song getHighestPopularitySong() {
+        if (songs.size() == 0) throw new EmptySongsListException("List of songs is empty");
 
         Song highestPopularitySong = songs.get(0);
-        for (Song song : songs){
-            if(song.popularity() > highestPopularitySong.popularity())
-                highestPopularitySong = song;
+        for (Song song : songs) {
+            if (song.popularity() > highestPopularitySong.popularity()) highestPopularitySong = song;
         }
         return highestPopularitySong;
     }
 
-    public Song getLongestSong(){
-        if(songs.size() == 0)
-            throw new RuntimeException("List of songs is empty");
+    public Song getLongestSong() {
+        if (songs.size() == 0) throw new RuntimeException("List of songs is empty");
 
         Song longestSong = songs.get(0);
-        for (Song song : songs){
-            if(song.length() > longestSong.popularity())
-                longestSong = song;
+        for (Song song : songs) {
+            if (song.length() > longestSong.popularity()) longestSong = song;
         }
         return longestSong;
     }
 }
 
-record Song(int id, String trackName, String artistName, String gender, double length, double popularity){}
+record Song(int id, String trackName, String artistName, String gender, double length, double popularity) {
+}
