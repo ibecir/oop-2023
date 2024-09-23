6c7157c6dbe4a2944f8e232c13975a099a865ea4
CausevicAmila <amila.causevic64@gmail.com>
2024-09-23 14:35:11 +0200

    week 1 labs

--- src/main/java/year2023/week1/labs/Introduction.java
+++ src/main/java/year2023/week1/labs/Introduction.java
@@ -5,41 +5,41 @@ import java.util.Scanner;
 public class Introduction {
     public static void main(String[] args) {
         // TASK 1
-        System.out.println("My name is Jane Doe");
+//        System.out.println("My name is Jane Doe");
         // TASK 2
-        System.out.println("Hello World! ");
-        System.out.println(" (And all the people of the world)");
+//        System.out.println("Hello World! ");
+//        System.out.println(" (And all the people of the world)");
         // TASK 3
-        System.out.println(" There are " + ( 365 * 24 * 60 * 60) + (" in a year."));
+//        System.out.println(" There are " + ( 365 * 24 * 60 * 60) + (" in a year."));
         // TASK 4
-        Scanner reader = new Scanner(System.in);
-        System.out.println("Type the first integer: ");
-        int firstNumber = Integer.parseInt(reader.nextLine());
-        System.out.println("Type the second integer: ");
-        int secondNumber = Integer.parseInt(reader.nextLine());
-        System.out.println("The sum of numbers is " + (firstNumber + secondNumber));
+//        Scanner reader = new Scanner(System.in);
+//        System.out.println("Type the first integer: ");
+//        int firstNumber = Integer.parseInt(reader.nextLine());
+//        System.out.println("Type the second integer: ");
+//        int secondNumber = Integer.parseInt(reader.nextLine());
+//        System.out.println("The sum of numbers is " + (firstNumber + secondNumber));
 //        TASK 5
-        Scanner reader = new Scanner(System.in);
-        System.out.println("Type an integer: ");
-        int number = Integer.parseInt(reader.nextLine());
-        if (number >= 0) {
-            System.out.println("The number is positive");
-        } else {
-            System.out.println("The number is negative");
-        }
+//        Scanner reader = new Scanner(System.in);
+//        System.out.println("Type an integer: ");
+//        int number = Integer.parseInt(reader.nextLine());
+//        if (number >= 0) {
+//            System.out.println("The number is positive");
+//        } else {
+//            System.out.println("The number is negative");
+//        }
 //        TASK 6
-        Scanner reader = new Scanner(System.in);
-        System.out.println("Type the first integer: ");
-        int firstNumber = Integer.parseInt(reader.nextLine());
-        System.out.println("Type the second integer: ");
-        int secondNumber = Integer.parseInt(reader.nextLine());
-
-        if (firstNumber == secondNumber) {
-            System.out.println("Equal numbers");
-        } else if (firstNumber > secondNumber) {
-            System.out.println("First number is greater");
-        } else {
-            System.out.println("Second number is greater");
-        }
+//        Scanner reader = new Scanner(System.in);
+//        System.out.println("Type the first integer: ");
+//        int firstNumber = Integer.parseInt(reader.nextLine());
+//        System.out.println("Type the second integer: ");
+//        int secondNumber = Integer.parseInt(reader.nextLine());
+//
+//        if (firstNumber == secondNumber) {
+//            System.out.println("Equal numbers");
+//        } else if (firstNumber > secondNumber) {
+//            System.out.println("First number is greater");
+//        } else {
+//            System.out.println("Second number is greater");
+//        }
     }
 }
